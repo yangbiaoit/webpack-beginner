b8a0af247ada46e3cb57783eb9ee464bdd05b3bf
biubiu <yb634246733@163.com>
2016-05-21 20:50:50 +0800

    main.js内容更新

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 document.write('study webpack by webpack-dev-server is ok  asdfadsf');
-document.write('  xxxxxxxxxx');
+document.write('  xxxxxxxxxx  asfads');
 document.write('     ---------- asfadshttps://segmentfault.com/a/1190000005118282');
 //https://segmentfault.com/a/1190000005118282
