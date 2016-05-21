304bfc3a52e78ab761fbf5814b19e0d0b8e66dcc
biubiu <yb634246733@163.com>
2016-05-21 22:09:18 +0800

    修改后文件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 document.write('study webpack by webpack-dev-server is ok  asdfadsf');
 document.write('  xxxxxxxxxx  asfads');
-document.write('     ---------- asfadshttps://segmentfault.com/a/1190000005118282');
+document.write('     ---------- asfadshttps://segmentfault.com/a/1190000005118282 1111111');
 //https://segmentfault.com/a/1190000005118282
