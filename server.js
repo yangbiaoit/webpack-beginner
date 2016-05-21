70ac0f4f28dee1d1c0879d0fbf4089df2667167c
biubiu <yb634246733@163.com>
2016-05-21 16:24:16 +0800

    更新server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config.js');
+
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,　　//开启热调试
